a11cfe6253c167c98d6e72483f120e1835b94e81
aladshaw3 <aladshaw3@gatech.edu>
2015-10-06 15:02:53 -0400

    Update reference manual and main

--- src/main.cpp
+++ src/main.cpp
@@ -21,10 +21,6 @@ int main(int argc, const char * argv[])
 	success = run_executable(argc, argv);
 	
 	//Space below for overrides of the run_executable function
-	//success = MACAW_TESTS();
-	//success = SHARK_SCENARIO("shark_input.yml");
-	//success = SHARK_TESTS();
-	//success = SKUA_SCENARIOS("Scenario_Kr_Xe.txt", "AdsorbentProperties_FMOFZn.txt", "AllComponentProperties_N2_O2_Ar_CO2_Kr_Xe.txt", "AllAdsorbateProperties_Kr_Xe.txt");
 	
 	std::cout << "Exit Code:\t" << success << std::endl;
 	return success;
