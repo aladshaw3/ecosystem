016b42f5c8b83f6a50a6fd58209be6e61495784a
U-lenovo-PC\Administrator <aladshaw3@outlook.com>
2015-11-30 11:24:51 -0500

    Update main

--- src/main.cpp
+++ src/main.cpp
@@ -14,14 +14,15 @@
 #include "ui.h"
 
 int main(int argc, const char * argv[])
-{	
+{
 	int success = 0;
-	
+
 	/// Command Line Interface: To Override, comment out this line and replace with your own run function
 	success = run_executable(argc, argv);
-	
+
 	//Space below for overrides of the run_executable function
-	
+	//success = LARK_TESTS();
+
 	std::cout << "Exit Code:\t" << success << std::endl;
 	return success;
 }
