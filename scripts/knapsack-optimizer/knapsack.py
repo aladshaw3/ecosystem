2f2f4cbb4c84efdbf578eae9be51bb2fd24a4857
aladshaw3 <aladshaw3@gatech.edu>
2019-05-01 13:57:45 -0400

    Added a 0-1 knapsack optimizer script in python. This will be used as part of the uncertainty analyses for DTRA related C/C++ modules.

---
+++ scripts/knapsack-optimizer/knapsack.py
@@ -0,0 +1,166 @@
+## Create Class Objects ##
+
+class Item(object):
+    #Item object: each item as a name, value, and unique id
+    tag = 0
+    def __init__(self):
+        self.name = " "             #Name of the object
+        self.value = 0              #Value associated with that object
+        self.id = Item.tag          #Item id tag (unique for each instance of the object)
+        Item.tag += 1
+    def __str__(self):
+        return self.name + ' (' + str(self.id) + '): <' + str(self.value) + '>'
+    def set_name(self, name):
+        self.name = name
+    def set_value(self, value):
+        self.value = value
+    def get_name(self):
+        return self.name
+    def get_value(self):
+        return self.value
+    def get_id(self):
+        return self.id
+
+class ZeroOneKnapsack(object):
+    #ZeroOneKnapsack object: solves the 0/1 knapsack for a
+    #                   generic set of objectives and constraints
+    def __init__(self):
+        self.obj_func = []          #Storage for the objective function (passed by user)
+        self.obj_args = []          #Arguments tuple for the objective function (passed by user)
+        self.constraint_func = []   #Storage for the constraint function (passed by user)
+        self.constraint_args = []   #Arguments tuple for the constraint function (passed by user)
+        self.whole_tree = True      #Boolean to force exhaustive search of tree
+    
+    # Function to set the exhaustive search boolean
+    def exhaustive_search(self, opt):
+        self.whole_tree = opt
+
+    # Function to register the objective function and it's arguments it needs
+    def register_objective_func(self, obj_func, *obj_args):
+        if (len(self.obj_func) == 0):
+            self.obj_func.append(obj_func)
+            self.obj_args.append(obj_args)
+        else:
+            self.obj_func[0] = obj_func
+            self.obj_args[0] = obj_args
+
+    # Function to register the constraints function and it's arguments it needs
+    def register_constraints(self, con_fun, *con_args):
+        if (len(self.constraint_func) == 0):
+            self.constraint_func.append(con_fun)
+            self.constraint_args.append(con_args)
+        else:
+            self.constraint_func[0] = con_fun
+            self.constraint_args[0] = con_args
+
+    ''' NOTE: Both the constraint function and objective function supplied by the user must accept 
+                two arguments: (i) list of items and (ii) tuple of other any other arguments.
+        
+              Return types for each function are to be expected as follows:
+                    Constraint Function --> returns boolean (True = constraints met)
+                    Objective Function --> returns scalar 
+                                                Postive Scalar == Maximization
+                                                Negative Scalar == Minimization 
+        '''
+    
+    def eval_constraints(self, list):
+        return self.constraint_func[0](list, *self.constraint_args)
+
+    def eval_objective_func(self, list):
+        return self.obj_func[0](list, *self.obj_args)
+
+    # Function to solve the optimization problem given the original list of items to consider
+    ##      NOTE: User should not pass any arguments for 'taken' as this is used only internally
+    def Optimize(self, list, taken=None):
+        if taken is None:
+            taken = []
+        #Check constraints, compute value, view list
+        status = self.eval_constraints(taken)
+        value = self.eval_objective_func(taken)
+        result = (value, taken, status)
+        
+        # Check for empty list
+        if list == []:
+            result = (value, taken, status)
+        else:
+            # If constraints are violated or search is exhaustive, force a move to left and right
+            if status == False or self.whole_tree == True:
+                violation = True
+                i = 0
+                #Continue to loop while constraints are still violated
+                while violation:
+                    try:
+                        nextItem = list[i]
+                    except:
+                        break
+                    i += 1
+                    #Create a copy of current taken list before adding new item
+                    not_taken = taken.copy()
+                    taken.append(nextItem)
+
+                    #Move down left branch
+                    (left_val, left_take, left_status) = self.Optimize(list[i:],taken)
+
+                    #Move down right branch
+                    (right_val, right_take, right_status) = self.Optimize(list[i:],not_taken)
+            
+                    #Check the results
+                    if (left_status == True) and (right_status == True):
+                        violation = False
+                        # Take largest value
+                        if right_val > left_val:
+                            result = (right_val, right_take, right_status)
+                        else:
+                            result = (left_val, left_take, left_status)
+                    elif (left_status == True) and (right_status == False):
+                        violation = False
+                        result = (left_val, left_take, left_status)
+                    elif (left_status == False) and (right_status == True):
+                        violation = False
+                        result = (right_val, right_take, right_status)
+                    else:
+                        # If constraints are still violated, then force another move
+                        violation = True
+                        if right_val > left_val:
+                            result = (right_val, right_take, right_status)
+                        else:
+                            result = (left_val, left_take, left_status)
+                #END While Loop
+
+            # If constraints are not violated and search is shallow, check left and check right
+            else:
+                #Create copies to pass to arguments
+                not_taken = taken.copy()
+                taken.append(list[0])
+            
+                #Peak left and right to look for a move violation
+                left_status = self.eval_constraints(taken)
+                right_status = self.eval_constraints(not_taken)
+                
+                # Right move valid, but left isn't, then move right
+                if left_status == False and right_status == True:
+                    (right_val, right_take, right_status) = self.Optimize(list[1:],not_taken)
+                    result = (right_val, right_take, right_status)
+                
+                # Left move valid, but right isn't, then move left
+                elif left_status == True and right_status == False:
+                    (left_val, left_take, left_status) = self.Optimize(list[1:],taken)
+                    result = (left_val, left_take, left_status)
+
+                # Both moves valid, then explore both branches
+                elif left_status == True and right_status == True:
+                    (left_val, left_take, left_status) = self.Optimize(list[1:],taken)
+                    (right_val, right_take, right_status) = self.Optimize(list[1:],not_taken)
+                
+                    # Take largest value
+                    if right_val > left_val:
+                        result = (right_val, right_take, right_status)
+                    else:
+                        result = (left_val, left_take, left_status)
+
+                # No moves valid, return current result
+                else:
+                    result = (value, taken, status)
+
+        return result
+    #End Optimization Routine
---
+++ scripts/knapsack-optimizer/knapsack_test.py
@@ -0,0 +1,182 @@
+#Debugging and interacting (put anywhere in code to stop execution for querying structures) (Type: exit() to continue from that point)
+#import code
+#code.interact(local=locals())
+
+## Testing ##
+
+import knapsack as ns
+
+def size_con(list, args):
+    a = 0
+    # Try catch block used to prevent error if args not initialized
+    try:
+        a = len(list)
+    except:
+        a = 0
+        print('catch')
+    if a < 1:
+        return False
+    else:
+        return True
+
+def max_obj(list, args):
+    total = 0
+    for obj in list:
+        total += obj.get_value()
+    return total
+
+def min_obj(list, args):
+    total = 0
+    for obj in list:
+        total -= obj.get_value()
+    return total
+
+#NOTE: args come in as Tuple, for this case, we only want first tuple item and that is a dictionary
+def calories_con(list, args):
+    cal = 0
+    for obj in list:
+        if obj.get_name() in args[0]:
+            cal += args[0][obj.get_name()]
+
+    if (cal < 750):
+        return True
+    else:
+        return False
+
+def score(list, args):
+    #args is a tuple with dictionary variables F, ps1 [a], ps2 [b], ps3 [c], ps4 [d], and ps5 [e]
+    sum1 = 0
+    sum2 = 0
+    for obj in list:
+        sum2 += obj.get_value()
+        if obj.get_name() in args[0]:
+            sum1 += args[0][obj.get_name()]*obj.get_value()
+    total = 0
+    try:
+        total = (60.0-sum2)*args[0]["F"] + sum1
+    except:
+        total = sum1
+    return total
+
+def score_con(list, args):
+    sum = 0
+    for obj in list:
+        sum += obj.get_value()
+    if sum >= 20:
+        return True
+    else:
+        return False
+
+
+print('\nTest 01')
+print('-------')
+a = ns.Item()
+a.set_name("a")
+a.set_value(1)
+b = ns.Item()
+b.set_name("b")
+b.set_value(2)
+c = ns.Item()
+c.set_name("c")
+c.set_value(0)
+
+list = [a,b,c]
+for obj in list:
+    print(obj)
+
+prob = ns.ZeroOneKnapsack()
+prob.register_constraints(size_con)
+prob.register_objective_func(min_obj)
+prob.exhaustive_search(False)
+(val, new_list, status) = prob.Optimize(list)
+
+print('\nAfter Optimization: Test 01 Results')
+print(val)
+print(status)
+print('\nTaken')
+for obj in new_list:
+    print(obj)
+
+
+### New Test ###
+print('\nTest 02')
+print('-------')
+foods = []
+for i in range(8):
+    foods.append(ns.Item())
+foods[0].set_name("wine")
+foods[0].set_value(89)
+foods[1].set_name("beer")
+foods[1].set_value(90)
+foods[2].set_name("pizza")
+foods[2].set_value(95)
+foods[3].set_name("burger")
+foods[3].set_value(100)
+foods[4].set_name("fries")
+foods[4].set_value(90)
+foods[5].set_name("cola")
+foods[5].set_value(79)
+foods[6].set_name("apple")
+foods[6].set_value(50)
+foods[7].set_name("donut")
+foods[7].set_value(10)
+
+for obj in foods:
+    print(obj)
+calories = {"wine":123,"beer":154,"pizza":258,"burger":354,"fries":365,"cola":150,"apple":95,"donut":195}
+
+prob2 = ns.ZeroOneKnapsack()
+prob2.register_constraints(calories_con, calories)
+prob2.register_objective_func(max_obj)
+prob2.exhaustive_search(False)
+(val2, new_list2, status2) = prob2.Optimize(foods)
+
+print('\nAfter Optimization: Test 02 Results')
+print(val2)
+print(status2)
+print('\nTaken')
+for obj in new_list2:
+    print(obj)
+
+### New Test ###
+print('\nTest 03')
+print('-------')
+w = []
+for i in range(5):
+    w.append(ns.Item())
+w[0].set_name("a")
+w[0].set_value(10)
+w[1].set_name("b")
+w[1].set_value(10)
+w[2].set_name("c")
+w[2].set_value(10)
+w[3].set_name("d")
+w[3].set_value(10)
+w[4].set_name("e")
+w[4].set_value(10)
+for obj in w:
+    print(obj)
+grades = {"F":60,"a":10,"b":20,"c":20,"d":10,"e":10}
+
+prob3 = ns.ZeroOneKnapsack()
+prob3.register_objective_func(score,grades)
+prob3.register_constraints(score_con)
+prob3.exhaustive_search(True)
+(val3, new_list3, status3) = prob3.Optimize(w)
+
+print('\nAfter Optimization: Test 03 Results')
+print(val3)
+print(status3)
+print('\nTaken')
+for obj in new_list3:
+    print(obj)
+
+# This makes no fucking sense!!!
+'''
+def test(arg, list = []):
+    list.append(arg)
+    return list
+
+print(test(1))  # result: [1]        expected: [1]
+print(test(2))  # result: [1, 2]     expected: [2]
+'''
