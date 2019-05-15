902b23650d079c72991e381375af86e828a52912
aladshaw3 <aladshaw3@gatech.edu>
2019-05-15 10:59:09 -0400

    Created control file editor script.

--- scripts/cardinal_uncertainty/control_reader.py
+++ scripts/cardinal_uncertainty/control_reader.py
@@ -24,6 +24,13 @@ class ControlParam(Enum):
     Yield  = 0
     Height = 1
     Level = 2
+    Wind = 3
+    Mass = 4
+    FissExtent = 5
+    FissYield = 6
+    PartDist = 7
+    Casing = 8
+    FracMod = 9
 
 class ControlFile(yr.YAML):
     def __init__(self):
@@ -32,10 +39,124 @@ class ControlFile(yr.YAML):
     def printMap(self):
         print(self.map)
 
+    def editValue_PercentChange(self, e_num, per):
+        if e_num == ControlParam.Yield:
+            self.map["Simulation_Conditions"]["bomb_yield"] = self.map["Simulation_Conditions"]["bomb_yield"]*(1.0 + (per/100.0))
+            if self.map["Simulation_Conditions"]["bomb_yield"] < 1.0:
+                self.map["Simulation_Conditions"]["bomb_yield"] = 1.0
+        if e_num == ControlParam.Height:
+            if self.map["Simulation_Conditions"]["burst_height"] == 0.0:
+                self.map["Simulation_Conditions"]["burst_height"] = per
+            else:
+                self.map["Simulation_Conditions"]["burst_height"] = self.map["Simulation_Conditions"]["burst_height"]*(1.0 + (per/100.0))
+        if e_num == ControlParam.Level:
+            if self.map["Simulation_Conditions"]["ground_level"] == 0.0:
+                self.map["Simulation_Conditions"]["ground_level"] = per
+            else:
+                self.map["Simulation_Conditions"]["ground_level"] = self.map["Simulation_Conditions"]["ground_level"]*(1.0 + (per/100.0))
+        if e_num == ControlParam.Wind:
+            for alt in self.map["Wind_Profile"]:
+                self.map["Wind_Profile"][alt]["vx"] = self.map["Wind_Profile"][alt]["vx"]*(1.0 + (per/100.0))
+                self.map["Wind_Profile"][alt]["vy"] = self.map["Wind_Profile"][alt]["vy"]*(1.0 + (per/100.0))
+        if e_num == ControlParam.Mass:
+            self.map["Weapon_Info"]["total_mass"] = self.map["Weapon_Info"]["total_mass"]*(1.0 + (per/100.0))
+            if self.map["Weapon_Info"]["total_mass"] < 1.0:
+                self.map["Weapon_Info"]["total_mass"] = 1.0
+        if e_num == ControlParam.FissExtent:
+            self.map["Weapon_Info"]["fission_extent"] = self.map["Weapon_Info"]["fission_extent"]*(1.0 + (per/100.0))
+            if self.map["Weapon_Info"]["fission_extent"] > 100:
+                self.map["Weapon_Info"]["fission_extent"] = 100.0
+            if self.map["Weapon_Info"]["fission_extent"] < 1:
+                self.map["Weapon_Info"]["fission_extent"] = 1.0
+        if e_num == ControlParam.FissYield:
+            self.map["Weapon_Info"]["fission_yield"] = self.map["Weapon_Info"]["fission_yield"]*(1.0 + (per/100.0))
+            if self.map["Weapon_Info"]["fission_yield"] > self.map["Simulation_Conditions"]["bomb_yield"]:
+                self.map["Weapon_Info"]["fission_yield"] = self.map["Simulation_Conditions"]["bomb_yield"]
+            if self.map["Weapon_Info"]["fission_yield"] < 0.0:
+                self.map["Weapon_Info"]["fission_yield"] = 1.0
+        if e_num == ControlParam.PartDist:
+            if self.map["Simulation_Conditions"]["part_dist"]["useCustom"] == True:
+                self.map["Simulation_Conditions"]["part_dist"]["min_dia"] = self.map["Simulation_Conditions"]["part_dist"]["min_dia"]*(1.0 + (per/100.0))
+                self.map["Simulation_Conditions"]["part_dist"]["max_dia"] = self.map["Simulation_Conditions"]["part_dist"]["max_dia"]*(1.0 + (per/100.0))
+                self.map["Simulation_Conditions"]["part_dist"]["mean_dia"] = self.map["Simulation_Conditions"]["part_dist"]["mean_dia"]*(1.0 + (per/100.0))
+                self.map["Simulation_Conditions"]["part_dist"]["std_dia"] = self.map["Simulation_Conditions"]["part_dist"]["std_dia"]*(1.0 + (per/100.0))
+        if e_num == ControlParam.Casing:
+            self.map["Weapon_Info"]["casing_thickness"] = self.map["Weapon_Info"]["casing_thickness"]*(1.0 + (per/100.0))
+            if self.map["Weapon_Info"]["casing_thickness"] < 1.0:
+                self.map["Weapon_Info"]["casing_thickness"] = 1.0
+        if e_num == ControlParam.FracMod:
+            if self.map["Weapon_Info"]["fractionation_model"] == "modified-freiling-tompkins":
+                self.map["Weapon_Info"]["fractionation_model"] = "freiling-tompkins"
+            elif self.map["Weapon_Info"]["fractionation_model"] == "freiling-tompkins":
+                self.map["Weapon_Info"]["fractionation_model"] = "modified-freiling-tompkins"
+            if self.map["Weapon_Info"]["fractionation_model"] == "freiling":
+                self.map["Weapon_Info"]["fractionation_model"] = "modified-freiling"
+            elif self.map["Weapon_Info"]["fractionation_model"] == "modified-freiling":
+                self.map["Weapon_Info"]["fractionation_model"] = "freiling"
+
+    def editValue_LinearChange(self, e_num, value):
+        if e_num == ControlParam.Yield:
+            self.map["Simulation_Conditions"]["bomb_yield"] = self.map["Simulation_Conditions"]["bomb_yield"] + value
+            if self.map["Simulation_Conditions"]["bomb_yield"] < 1.0:
+                self.map["Simulation_Conditions"]["bomb_yield"] = 1.0
+        if e_num == ControlParam.Height:
+            if self.map["Simulation_Conditions"]["burst_height"] == 0.0:
+                self.map["Simulation_Conditions"]["burst_height"] = value
+            else:
+                self.map["Simulation_Conditions"]["burst_height"] = self.map["Simulation_Conditions"]["burst_height"] + value
+        if e_num == ControlParam.Level:
+            if self.map["Simulation_Conditions"]["ground_level"] == 0.0:
+                self.map["Simulation_Conditions"]["ground_level"] = value
+            else:
+                self.map["Simulation_Conditions"]["ground_level"] = self.map["Simulation_Conditions"]["ground_level"] + value
+        if e_num == ControlParam.Wind:
+            for alt in self.map["Wind_Profile"]:
+                self.map["Wind_Profile"][alt]["vx"] = self.map["Wind_Profile"][alt]["vx"] + value
+                self.map["Wind_Profile"][alt]["vy"] = self.map["Wind_Profile"][alt]["vy"] + value
+        if e_num == ControlParam.Mass:
+            self.map["Weapon_Info"]["total_mass"] = self.map["Weapon_Info"]["total_mass"] + value
+            if self.map["Weapon_Info"]["total_mass"] < 1.0:
+                self.map["Weapon_Info"]["total_mass"] = 1.0
+        if e_num == ControlParam.FissExtent:
+            self.map["Weapon_Info"]["fission_extent"] = self.map["Weapon_Info"]["fission_extent"] + value
+            if self.map["Weapon_Info"]["fission_extent"] > 100:
+                self.map["Weapon_Info"]["fission_extent"] = 100.0
+            if self.map["Weapon_Info"]["fission_extent"] < 1:
+                self.map["Weapon_Info"]["fission_extent"] = 1.0
+        if e_num == ControlParam.FissYield:
+            self.map["Weapon_Info"]["fission_yield"] = self.map["Weapon_Info"]["fission_yield"] + value
+            if self.map["Weapon_Info"]["fission_yield"] > self.map["Simulation_Conditions"]["bomb_yield"]:
+                self.map["Weapon_Info"]["fission_yield"] = self.map["Simulation_Conditions"]["bomb_yield"]
+            if self.map["Weapon_Info"]["fission_yield"] < 0.0:
+                self.map["Weapon_Info"]["fission_yield"] = 1.0
+        if e_num == ControlParam.PartDist:
+            if self.map["Simulation_Conditions"]["part_dist"]["useCustom"] == True:
+                self.map["Simulation_Conditions"]["part_dist"]["min_dia"] = self.map["Simulation_Conditions"]["part_dist"]["min_dia"] + value
+                self.map["Simulation_Conditions"]["part_dist"]["max_dia"] = self.map["Simulation_Conditions"]["part_dist"]["max_dia"] + value
+                self.map["Simulation_Conditions"]["part_dist"]["mean_dia"] = self.map["Simulation_Conditions"]["part_dist"]["mean_dia"] + value
+                self.map["Simulation_Conditions"]["part_dist"]["std_dia"] = self.map["Simulation_Conditions"]["part_dist"]["std_dia"] + value
+        if e_num == ControlParam.Casing:
+            self.map["Weapon_Info"]["casing_thickness"] = self.map["Weapon_Info"]["casing_thickness"] + value
+            if self.map["Weapon_Info"]["casing_thickness"] < 1.0:
+                self.map["Weapon_Info"]["casing_thickness"] = 1.0
+        if e_num == ControlParam.FracMod:
+            if self.map["Weapon_Info"]["fractionation_model"] == "modified-freiling-tompkins":
+                self.map["Weapon_Info"]["fractionation_model"] = "freiling-tompkins"
+            elif self.map["Weapon_Info"]["fractionation_model"] == "freiling-tompkins":
+                self.map["Weapon_Info"]["fractionation_model"] = "modified-freiling-tompkins"
+            if self.map["Weapon_Info"]["fractionation_model"] == "freiling":
+                self.map["Weapon_Info"]["fractionation_model"] = "modified-freiling"
+            elif self.map["Weapon_Info"]["fractionation_model"] == "modified-freiling":
+                self.map["Weapon_Info"]["fractionation_model"] = "freiling"
+
+    def clearMap(self):
+        self.map.clear()
 
 ## Testing ##
+'''
 control = ControlFile()
 control.readFile("1979-Test-Case.txt")
-#print(control)
-control.printMap()
+control.editValue_LinearChange(ControlParam.Mass, 10.0)
+control.print2file("1979-Test-Case-Mod.txt")
+'''
 
