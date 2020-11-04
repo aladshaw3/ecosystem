9fe5d6bd85aa3048ba2a51af092cc1b679546173
aladshaw3 <1pi@ornl.gov>
2020-11-04 13:56:32 -0500

    Added analysis for 1-propanol

--- scripts/CoOptima/PyomoReactionModel/reaction_model_framework.py
+++ scripts/CoOptima/PyomoReactionModel/reaction_model_framework.py
@@ -212,7 +212,7 @@ class ReactionModel(object):
                 lock_B = False
                 lock_E = False
 
-                # TODO: Figure out a way to lock specific variables during optimization 
+                # TODO: Figure out a way to lock specific variables during optimization
 
                 try:
                     if isinstance(doc["Reaction_Params"][rxn]["unbound_A"], bool):
@@ -451,7 +451,7 @@ class ReactionModel(object):
             solver.options['max_iter'] = 20*self.total_var
             solver.options['obj_scaling_factor'] = 1 #Set scaling factor to value similar to tol?
             if self.simulate_only == False:
-                solver.options['diverging_iterates_tol'] = 1e60
+                solver.options['diverging_iterates_tol'] = 1e70
             results = solver.solve(self.instance, tee=True, load_solutions=False)
             try:
                 self.instance.solutions.load_from(results)
